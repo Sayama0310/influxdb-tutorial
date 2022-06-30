01e4efe04473158bc5f8750b48dea5adb0d8d869
YuseiSayama <sayama_yusei@interprism.co.jp>
2022-06-30 16:54:52 +0900

    change the InfluxClient as class and prepare write_data method.

--- __init__.py
+++ __init__.py
@@ -4,11 +4,22 @@ import os
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
+
+class InfluxClient:
+    def __init__(self, token, org, bucket):
+        self._org = org
+        self._bucket = bucket
+        self._client = InfluxDBClient(url="http://localhost:8086", token=token)
+
+    def write_data(self, data, write_option=SYNCHRONOUS):
+        write_api = self._client.write_api(write_option)
+        write_api.write(self._bucket, self._org, data)
+
+
 load_dotenv()
 # You can generate a Token from the "Tokens Tab" in the UI
-token = os.getenv('TOKEN')
-org = os.getenv('ORG')
-bucket = os.getenv('BUCKET')
+TOKEN = os.getenv('TOKEN')
+ORG = os.getenv('ORG')
+BUCKET = os.getenv('BUCKET')
 
-client = InfluxDBClient(url="http://localhost:8086", token=token)
-
+client = InfluxClient(TOKEN, ORG, BUCKET)
